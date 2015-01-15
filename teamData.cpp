4a9a0e43fe9bcf43c01c99e7ce2bf968983829a0
Peter Sutton <p.sutton@uq.edu.au>
2015-01-15 22:33:31 +1000

    Initial (incomplete) versions of team Data related classes and functions

---
+++ teamData.cpp
@@ -0,0 +1,207 @@
+//
+// teamData.cpp
+//
+
+#include "teamData.hh"
+#include "attribute.hh"
+#include <exception>
+#include "assert.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// Entity
+
+// Constructor
+Entity::Entity(Entity::Type type) :
+	type(type)
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Member
+
+// Constructor
+Member::Member(Person& person) :
+	Entity(Entity::MEMBER),
+	person(person)
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// TeamLevel
+
+// Constructor
+TeamLevel::TeamLevel(int level) :
+	Entity(Entity::TEAM),
+	level(level)
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// Partition
+
+// Constructor
+Partition::Partition(const AllTeamData& allTeamData, const string& name, int numPeople) :
+	Entity(Entity::PARTITION),
+	allTeamData(allTeamData),
+	name(name),
+	cost(0.0),
+	bestCost(0.0)
+{
+    allMembers.reserve(numPeople);
+}
+
+// Other member functions
+void Partition::add_member(Member* member)
+{
+    allMembers.push_back(member);
+}
+
+void Partition::populate_random_teams()
+{
+    vector<Level*>& allLevels = allTeamData.allLevels;
+
+    vector<Entity*> allEntities = allMembers;
+    assert(highestLevelTeams.size() == 0);
+    assert(lowestLevelTeams.size() == 0);
+    // Work backwards over our levels - start at the lowest level (individual people)
+    for(vector<Level*>::reverse_iterator rit = allLevels.rbegin(); rit != allLevels.rend(); ++rit) {
+	// Work out the number of teams we'll have
+	//int numEntitiesInTeams = allEntities.size();
+	//int numTeams = AllTeamData::number_of_teams(numEntitiesInTeams, (*rit)->minSize,
+		//(*rit)->idealSize, (*rit)->maxSize);
+    }
+}
+
+void Partition::populate_existing_teams()
+{
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// AllTeamData
+
+// Constructor
+AllTeamData::AllTeamData(AnnealInfo& annealInfo) :
+	allLevels(annealInfo.all_levels())
+{
+    Partition* partition;
+
+    // Create partitions
+    Attribute* partitionAttribute = annealInfo.get_partition_field();
+
+    if(partitionAttribute) {
+	// Iterate over all the values for this attribute and create a partition
+	// for each
+	for(Attribute::ValueIterator itr = partitionAttribute->iterator(); 
+		itr != partitionAttribute->end(); ++itr) {
+	    // itr will be a const string*
+	    // Create a new partition with the given name
+	    partitionMap.insert(pair<const string,Partition*>(*itr, new Partition(annealInfo,
+		    *itr, 
+		    annealInfo.count_people_with_attribute_value(partitionAttribute, *itr))));
+	}
+    } else {
+	// No partitions - just create one with an empty name
+	partition = new Partition(annealInfo, "", annealInfo.num_people());
+	partitionMap.insert(pair<const string, Partition*>("", partition));
+    }
+
+    // Reserve space for the members we will have
+    allMembers.reserve(annealInfo.num_people());
+
+    // Iterate over all the people to build up our list of members
+    for(vector<Person*>::iterator it = annealInfo.all_people().begin();
+	    it != annealInfo.all_people().end(); ++it) {
+	Member* member = new Member(**it);
+	allMembers.push_back(member);
+	// Work out which partition they go in
+	if(partitionAttribute) {
+	    partition = find_partition((*it)->get_string_attribute_value(partitionAttribute));
+	} // else, no partitions. partition variable already holds a pointer to our one dummy partition
+	partition->add_member(member);
+    }
+}
+
+void AllTeamData::populate_random_teams()
+{
+    // Iterate over each partition
+    for(map<string,Partition*>::iterator it = partitionMap.begin(); it != partitionMap.end(); ++it) {
+	it->second->populate_random_teams();
+    }
+}
+
+void AllTeamData::populate_existing_teams()
+{
+    // Iterate over each partition
+    for(map<string,Partition*>::iterator it = partitionMap.begin(); it != partitionMap.end(); ++it) {
+	it->second->populate_existing_teams();
+    }
+}
+
+Partition* AllTeamData::find_partition(const string& name)
+{
+    map<string,Partition*>::iterator it;
+    it = partitionMap.find(name);
+    if(it == partitionMap.end()) {
+	// Partition not found
+	return nullptr;
+    } else {
+	return it->second;
+    }
+}
+
+// static member
+int AllTeamData::number_of_teams(int numMembers, int minSize, int idealSize, 
+	int maxSize, bool favourSmaller)
+{
+    // Default value for numTeams may get adjusted as we go
+    int numTeams = numMembers / idealSize;	// number of ideal size teams
+    int numLeftOver = numMembers % idealSize;	// number of people left over
+
+
+    if(numLeftOver == 0) {
+	// All teams can be ideal size 
+    } else if(minSize == idealSize && idealSize == maxSize) {
+	// overconstrained - throw Exception
+	throw ("Not all teams can be the ideal size");
+    } else if(minSize == idealSize && idealSize < maxSize) {
+	// Teams can be larger than ideal but not smaller. Excess members
+	// will get distributed amongst existing teams
+    } else if(minSize < idealSize && idealSize == maxSize) {
+	// Teams can be smaller but not larger. We'll need one additional team
+	// to hold our excess members + some taken from other teams
+	numTeams++;
+    } else {
+	// Possibly underconstrained - we can have larger or smaller teams. We
+	// favour minimising the number of non-ideal teams and making sizes as 
+	// close to ideal as possible (plus or minus 1). NOTE: We ignore the 
+	// possibility of teams being more than 1 different from the ideal
+	if(idealSize % 2 == 0 && numLeftOver == (idealSize / 2)) {
+	    //We have exactly half a team left over - we could go smaller or larger
+	    if(favourSmaller && ((minSize - numLeftOver) <= numTeams)) {
+		// We can extract one from each of other teams to make up the difference
+		numTeams++;
+	    } else if(numMembers > numTeams * maxSize) {
+		// Can't fit everyone in maximum size teams - try adding one more team
+		numTeams++;
+	    } else {
+		// go larger - Excess team members will get distributed amongst
+		// existing teams
+	    }
+	} else if(numLeftOver > (idealSize / 2) && ((minSize - numLeftOver) <= numTeams)) {
+	    // We have more than half a team worth of members left over and we have enough teams
+	    // to take one member each from - favour making teams smaller
+	    numTeams++;
+	} else if(numMembers > numTeams * maxSize) {
+	    // Can't fit everyone in maximum size teams - try adding one more
+	    numTeams++;
+	} else {
+	    // Favour making teams larger - left over team members distributed 
+	    // amongst other teams
+	}
+    }
+    if(numMembers < numTeams * minSize || numMembers > numTeams * maxSize) {
+	// We can't make this work - throw an exception to indicate error
+	throw ("Can't meet team size contraints");
+    }
+    return numTeams;
+}
---
+++ teamData.hh
@@ -0,0 +1,95 @@
+//
+// teamData.hh
+//
+
+#ifndef TEAMDATA_HH
+#define TEAMDATA_HH
+
+#include "person.hh"
+#include "annealinfo.hh"
+#include <vector>
+#include <map>
+
+using namespace std;
+
+class AllTeamData;
+
+class Entity {
+public:
+    enum Type { MEMBER, TEAM, PARTITION };
+    Entity::Type type;
+
+    // Constructor
+    Entity(Entity::Type type);
+};
+
+class Member : public Entity {
+public:
+    Person& person;	// links to attributes of this person
+    vector<int> conditionMet;	// one entry for each constraint. If the constraint is a count
+    				// constraint then this is 1 if the field-operator-value is true
+				// e.g. 1 if "GPA > 5", 0 otherwise
+				// Value ignored for non-count constraints.
+				// (Indexed by constraint number 0 to num constraints minus 1
+    // Constructor
+    Member(Person& person);
+};
+
+class TeamLevel : public Entity {
+public:
+    // Contains either another level of teams or individual members
+    vector<Entity*> members;
+    int level;
+
+    // Constructor
+    TeamLevel(int level);
+};
+
+class Partition : public Entity {
+public:
+    const AllTeamData& 	allTeamData;
+    const string& 	name;		// empty if no partitions - values of the partition field
+    vector<TeamLevel*> 	lowestLevelTeams;
+    vector<TeamLevel*> 	highestLevelTeams;
+    vector<TeamLevel*> 	bestCostTeams;
+    vector<Entity*> 	allMembers;
+    vector<Member*> 	unallocatedMembers;
+    double cost;
+    double bestCost;
+
+    // Constructor
+    Partition(const AllTeamData& allTeamData, const string& name, int numPeople);
+
+    // Other member functions
+    // Add a member to our partition. All members are added before teams are formed.
+    void add_member(Member* member);
+
+    void populate_random_teams();
+    void populate_existing_teams();
+};
+
+class AllTeamData {
+public:
+    vector<Level*>& allLevels;
+    map<string,Partition*> partitionMap;
+    vector<Member*> allMembers;
+
+public:
+    // Constructor - initialises our list of partitions and list of all members.
+    AllTeamData(AnnealInfo& annealInfo);
+
+    // Member functions
+    void populate_random_teams();
+    void populate_existing_teams();
+
+    Partition* find_partition(const string& name);
+
+    // Other functions
+    // Returns the number of teams needed given the size constraints. Throws an exception 
+    // if not possible to meet the constraints
+
+    static int number_of_teams(int numMembers, int minSize, int idealSize, 
+	    int maxSize, bool favourSmaller = false);
+};
+
+#endif
