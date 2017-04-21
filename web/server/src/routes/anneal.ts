a30f88b4170f8f258cce285f2f37e72584191875
James Li <james.li1@uq.edu.au>
2017-04-21 11:41:14 +1000

    Starting to interact with POST body

--- web/server/src/routes/anneal.ts
+++ web/server/src/routes/anneal.ts
@@ -5,14 +5,31 @@ module.exports = () => {
     const router = express.Router();
 
     router.route("/")
-        .get((_req, res, _next) => {
-            return res.status(200).send("Anneal API");
-        });
-
-
-
-
-
+        .post(PostDataAnnealHandler);
 
     return router;
 }
+
+const PostDataAnnealHandler: express.RequestHandler =
+    (req, res, _next) => {
+        interface IncomingData {
+            readonly sourceData: {
+                readonly headers: ReadonlyArray<string>,
+                readonly types: ReadonlyArray<number>,
+                readonly records: ReadonlyArray<ReadonlyArray<number | string>>,
+            },
+            readonly constraints: {
+
+            }
+        }
+
+        const data: IncomingData = req.body;
+
+
+
+        return res
+            .status(200)
+            .json({
+                numberOfRecords: data.sourceData.records.length,
+            });
+    };
