26b3d38dc7a24f969377b946b8fe57cd82be9204
James Li <james.li1@uq.edu.au>
2017-05-04 16:33:34 +1000

    `null` is a permitted record element value

--- web/common/Record.ts
+++ web/common/Record.ts
@@ -1,4 +1,4 @@
 /** Element value in a record */
-export type RecordElement = number | string;
+export type RecordElement = number | string | null;
 /** A record is an array of values (elements) */
 export type Record = ReadonlyArray<RecordElement>;
