e730402d391016d201e79226828e04fc35dca05f
James Li <james.li1@uq.edu.au>
2017-05-09 10:47:05 +1000

    RecordSet type

--- web/common/Record.ts
+++ web/common/Record.ts
@@ -2,3 +2,5 @@
 export type RecordElement = number | string | null;
 /** A record is an array of values (elements) */
 export type Record = ReadonlyArray<RecordElement>;
+/** A set of records */
+export type RecordSet = ReadonlyArray<Record>;
