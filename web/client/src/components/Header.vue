2014261065839084865384e7e9053b1ea50d8425
James Li <james.li1@uq.edu.au>
2018-03-23 13:44:01 +1000

    Early header work

--- web/client/src/components/Header.vue
+++ web/client/src/components/Header.vue
@@ -1,30 +1,45 @@
 <template>
-    <div id="teamanneal">
-        <Header :enableMenu="headerOptions.enableMenu" :minimal="headerOptions.minimal" :headerText="headerOptions.headerText"></Header>
-        <div id="content">
-            <router-view />
-        </div>
+    <div class="header"
+         :class="headerClasses">
+
+        <h1 v-if="!enableMenu">
+            <a :class="logoClasses"
+               href="#">The University of Queensland</a>
+        </h1>
+        <HeaderMenu :enableMenu="enableMenu"></HeaderMenu>
+        
+        <h2>
+            {{headerText}}
+        </h2>
     </div>
 </template>
 
 <!-- ####################################################################### -->
 
 <script lang="ts">
-import { Vue, Component } from "av-ts";
-import Header from "./Header.vue";
+import { Vue, Component, Prop, p } from "av-ts";
+import HeaderMenu from "./HeaderMenu.vue";
 
 @Component({
     components: {
-        Header
+        HeaderMenu
     }
 })
-export default class TeamAnneal extends Vue {
-    
-    get headerOptions() {
+export default class Header extends Vue {
+    @Prop headerText = p<string>({ required: false, default: 'TeamAnneal' });
+    @Prop minimal = p<boolean>({ required: false, default: () => false });
+    @Prop enableMenu = p<boolean>({ required: false, default: () => true });
+
+
+    get headerClasses() {
         return {
-            headerText: 'TeamAnneal',
-            minimal: true,
-            enableMenu: true
+            'header-minimal': this.minimal
+        }
+    }
+
+    get logoClasses() {
+        return {
+            "uq-logo": !this.enableMenu
         }
     }
 }
@@ -33,15 +48,7 @@ export default class TeamAnneal extends Vue {
 <!-- ####################################################################### -->
 
 <style scoped>
-#teamanneal {
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: column;
-}
-
-#header {
+.header {
     background-color: #49075E;
     padding: 1rem 1.6rem;
     font-size: 0.8rem;
@@ -56,8 +63,14 @@ export default class TeamAnneal extends Vue {
     align-items: center;
 }
 
-#header h1,
-#header h2 {
+.header-minimal {
+    padding: 0.3rem 0rem;
+}
+
+
+
+.header h1,
+.header h2 {
     font-weight: normal;
     color: inherit;
     margin: 0;
@@ -65,18 +78,18 @@ export default class TeamAnneal extends Vue {
     border: 0;
 }
 
-#header h1 {
+.header h1 {
     padding: 0.4rem 1rem;
     border-right: 0.1rem solid rgba(255, 255, 255, 0.15);
     margin-right: 1.2rem;
 }
 
-#header h2 {
+.header h2 {
     font-size: 1.8em;
     font-weight: 300;
 }
 
-#uq-logo {
+.uq-logo {
     background: url(https://static.uq.net.au/v3/logos/corporate/uq-logo-white.png) no-repeat 0 0;
     background-image: url(https://static.uq.net.au/v3/logos/corporate/uq-logo-white.svg);
     background-size: 178px 50px;
@@ -88,20 +101,7 @@ export default class TeamAnneal extends Vue {
     padding: 0;
 }
 
-
-
-
-
-#content {
-    flex-shrink: 0;
-    flex-grow: 1;
-
-    position: relative;
-
-    background: #f5f5f5;
-    background-image: linear-gradient(to top, rgba(73, 7, 94, 0.5), rgba(73, 7, 94, 0.1)), url(../static/bonding-1985863_1920.jpg);
-    background-size: cover;
-    background-repeat: no-repeat;
-    background-position: center center;
+.menu {
+    cursor: pointer;
 }
 </style>
