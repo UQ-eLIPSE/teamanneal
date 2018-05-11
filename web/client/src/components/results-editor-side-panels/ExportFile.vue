8358933f2f990789e495c7f6cf95d61ec4cdb5c0
James Li <james.li1@uq.edu.au>
2018-05-11 16:16:11 +1000

    Combined name setting input in CSV export

--- web/client/src/components/results-editor-side-panels/ExportFile.vue
+++ web/client/src/components/results-editor-side-panels/ExportFile.vue
@@ -16,6 +16,37 @@
             <p>This file can be opened in any spreadsheet application.</p>
             <button class="button small gold"
                     @click="onExportCsvButtonClick">Export CSV</button>
+            <button class="button small secondary">Advanced export options</button>
+
+            <div class="export-option-block">
+                <h4>Combined name column</h4>
+                <p>You can choose to include a column in the output CSV that combines the individual names from each level into one.</p>
+                <p>
+                    <label><input type="checkbox"
+                               v-model="p_enableGroupCombinedName">Include combined name column</label>
+                </p>
+                <p>
+                    <table class="example-table">
+                        <tr>
+                            <th>Group level</th>
+                            <th>Placeholder to use</th>
+                        </tr>
+                        <tr v-for="item in groupCombinedNameFormatPlaceholderList"
+                            :key="item.label">
+                            <td>{{ item.label }}</td>
+                            <td>{{ item.placeholder }}</td>
+                        </tr>
+                    </table>
+                </p>
+                <p>
+                    <input class="combined-name-format"
+                           v-model="groupCombinedNameFormat"></input>
+                </p>
+                <p>
+                    For example:
+                    <i>{{ randomNodePathFormattedCombinedName }}</i>
+                </p>
+            </div>
         </div>
         <!-- TODO: Only show this block when anneal config detected -->
         <div class="export-option-block">
@@ -32,13 +63,22 @@
 
 <script lang="ts">
 import { Vue, Component } from "av-ts";
+import * as FileSaver from "file-saver";
 
 import { ResultsEditor as S } from "../../store";
 
-import * as FileSaver from "file-saver";
+import { ColumnData } from "../../data/ColumnData";
+import { GroupNode } from "../../data/GroupNode";
+import * as Stratum from "../../data/Stratum";
+
+import { pickRandom, reverse } from "../../util/Array";
+import { replaceAll } from "../../util/String";
 
 @Component
 export default class ExportFile extends Vue {
+    p_enableGroupCombinedName: boolean = true;
+    p_groupCombinedNameFormat: string = "";
+
     async onExportResultsPackageButtonClick() {
         // Get state serialised and save as TeamAnneal results package 
         // (just a JSON file internally)
@@ -58,6 +98,148 @@ export default class ExportFile extends Vue {
         // TODO: Implement exporting
         throw new Error("Not implemented");
     }
+
+    /** 
+     * Gets a random node path for the purposes of generating an example
+     * combined name
+     */
+    get randomNodePath() {
+        // Path is only a node ID path
+        const path: string[] = [];
+
+        function walkRandomPath(nodes: ReadonlyArray<GroupNode>): void {
+            const randomNode = pickRandom(nodes);
+
+            if (randomNode === undefined) {
+                console.error("Node not found");
+                return;
+            }
+
+            path.push(randomNode._id);
+
+            switch (randomNode.type) {
+                case "root":
+                case "intermediate-stratum":
+                    return walkRandomPath(randomNode.children);
+
+                case "leaf-stratum":
+                    // Can't go further
+                    return;
+            }
+        }
+
+        walkRandomPath(S.state.groupNode.structure.roots);
+
+        return path;
+    }
+
+    get randomNodePathFormattedCombinedName() {
+        const nameMap = S.state.groupNode.nameMap;
+        const randomNodePath = this.randomNodePath;
+
+        let combinedName = this.p_groupCombinedNameFormat;
+
+        // Set partition name
+        combinedName = replaceAll(combinedName, "{{_PARTITION}}", nameMap[randomNodePath[0]]);
+
+        // Set stratum names
+        //
+        // Strata are ordered [lowest/leaf, ..., highest] whereas the random
+        // path is [highest, ..., lowest/leaf]
+        reverse(this.strata).forEach((stratum, i) => {
+            combinedName = replaceAll(combinedName!, `{{${stratum._id}}}`, nameMap[randomNodePath[i + 1]]);
+        });
+
+        return combinedName;
+    }
+    get columns() {
+        return S.state.recordData.columns;
+    }
+    get strata() {
+        return S.state.strataConfig.strata;
+    }
+
+    get partitionColumnDescriptor() {
+        return S.state.recordData.partitionColumn;
+    }
+
+    get partitionColumn() {
+        const partitionColumnDesc = this.partitionColumnDescriptor;
+
+        if (partitionColumnDesc === undefined) {
+            return undefined;
+        }
+
+        return ColumnData.ConvertToDataObject(this.columns, partitionColumnDesc);
+    }
+
+    /**
+     * Determines if a partition column is set
+     */
+    get isPartitionColumnSet() {
+        return this.partitionColumnDescriptor !== undefined;
+    }
+
+    /**
+     * Returns a shim object that projects the partition as stratum
+     */
+    get partitionStratumShimObject() {
+        const partitionColumn = this.partitionColumnDescriptor;
+
+        if (partitionColumn === undefined) {
+            throw new Error("No partition column set");
+        }
+
+        const shimLabel = `Partition (${partitionColumn.label})`;
+
+        return Stratum.init(shimLabel);
+    }
+
+    get groupCombinedNameFormatPlaceholderList() {
+        const list = this.strata.map((stratum) => {
+            return {
+                label: stratum.label,
+                placeholder: `{{${stratum.label}}}`,
+            };
+        });
+
+        if (this.isPartitionColumnSet) {
+            const partitionShimObject = this.partitionStratumShimObject;
+
+            list.unshift({
+                label: partitionShimObject.label,
+                placeholder: "{{Partition}}",
+            });
+        }
+
+        return list;
+    }
+
+    get groupCombinedNameFormat() {
+        let format = this.p_groupCombinedNameFormat;
+
+        // Get stratum labels back out because internally we use IDs
+        this.strata.forEach(({ _id, label, }) => {
+            format = replaceAll(format!, `{{${_id}}}`, `{{${label}}}`);
+        });
+
+        // Internally we use _PARTITION to represent the partition column
+        format = replaceAll(format, "{{_PARTITION}}", "{{Partition}}");
+
+        return format;
+    }
+
+    set groupCombinedNameFormat(newValue: string) {
+        // Replace stratum labels with stratum IDs because internally we use IDs
+        this.strata.forEach(({ _id, label, }) => {
+            newValue = replaceAll(newValue!, `{{${label}}}`, `{{${_id}}}`);
+        });
+
+        // Internally we use _PARTITION to represent the partition column
+        newValue = replaceAll(newValue, "{{Partition}}", "{{_PARTITION}}");
+
+        this.p_groupCombinedNameFormat = newValue;
+    }
 }
 </script>
 
@@ -71,10 +253,14 @@ export default class ExportFile extends Vue {
     background: rgba(73, 7, 94, 0.1);
 
     padding: 1em;
-    margin-bottom: 1em;
 }
 
-.export-option-block>h3 {
+.export-option-block+.export-option-block {
+    margin-top: 1em;
+}
+
+.export-option-block>h3,
+.export-option-block>h4 {
     color: #49075E;
     margin-top: 0;
 }
