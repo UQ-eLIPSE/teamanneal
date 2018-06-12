7f2f072bf610236a9430f37465ae79b751c47a44
James Li <james.li1@uq.edu.au>
2018-06-12 12:25:44 +1000

    Usable "Pool records"

--- web/client/src/components/anneal-wizard-panels/PoolRecords.vue
+++ web/client/src/components/anneal-wizard-panels/PoolRecords.vue
@@ -4,7 +4,7 @@
             <h1>Pool records</h1>
             <!-- TODO: Add help box back -->
             <p>
-                Choose to either use all records in one pool or split them into separate pools when forming teams.
+                Choose either to use all records or split them into separate pools before forming groups.
             </p>
             <p>
                 For example, if you have students which are associated with certain
@@ -12,13 +12,32 @@
                 <i>project group</i> value, you would set the
                 <i>"project group"</i> column to use for pooling your data.
             </p>
-            <p>
-                <select v-model="partitionColumn">
-                    <option v-for="(option, i) in possiblePartitionColumns"
-                            :key="option.text + i"
-                            :value="option.value">{{ option.text }}</option>
-                </select>
-            </p>
+            <div class="pool-records-split-pane-wrapper">
+                <div class="pool-records-split-pane">
+                    <label class="pool-records-section-box"
+                           :class="{ 'deselected': enablePartitioning === true }">
+                        <h3><input type="radio"
+                                   v-model="enablePartitioning"
+                                   :value="false"> Use all records across all groups</h3>
+                        <p>All records will be used to form groups.</p>
+                    </label>
+                    <label class="pool-records-section-box"
+                           :class="{ 'deselected': enablePartitioning === false }">
+                        <h3><input type="radio"
+                                   v-model="enablePartitioning"
+                                   :value="true"> Split records into separate pools</h3>
+                        <p>Records will be split into pools according to the selected column before forming groups.</p>
+                        <p>
+                            <select v-model="partitionColumn"
+                                    :disabled="enablePartitioning === false">
+                                <option v-for="(option, i) in formattedPartitionColumnList"
+                                        :key="(option.value || {})._id"
+                                        :value="option.value">{{ option.text }}</option>
+                            </select>
+                        </p>
+                    </label>
+                </div>
+            </div>
         </div>
         <div class="wizard-panel-bottom-buttons">
             <button class="button"
@@ -31,7 +50,7 @@
 <!-- ####################################################################### -->
 
 <script lang="ts">
-import { Component, Mixin } from "av-ts";
+import { Component, Mixin, Lifecycle } from "av-ts";
 
 import { ColumnData, MinimalDescriptor as IColumnData_MinimalDescriptor } from "../../data/ColumnData";
 import * as AnnealProcessWizardEntries from "../../data/AnnealProcessWizardEntries";
@@ -46,47 +65,107 @@ export default class PoolRecords extends Mixin(AnnealProcessWizardPanel) {
     // Defines the wizard step
     readonly thisWizardStep = AnnealProcessWizardEntries.poolRecords;
 
-    setPartitionColumn() {
-        // Partition should already be set reactively; just move on to next step
-        this.emitWizardNavNext();
+    /** 
+     * Temporarily stores last set partition column to prevent the <select>
+     * menu from having a value which changes unexpectedly
+     */
+    lastSetColumn: IColumnData_MinimalDescriptor | undefined = undefined;
+
+    get columns() {
+        return S.state.recordData.columns;
     }
 
-    async skipPartitioning() {
-        // Delete partition column
-        await S.dispatch(S.action.CLEAR_RECORD_PARTITION_COLUMN, undefined);
-
-        this.emitWizardNavNext();
+    get numberOfRecords() {
+        return S.state.recordData.source.length - 1;
     }
 
     get possiblePartitionColumns() {
-        const recordData = S.state.recordData;
-        const columns = recordData.columns;
-        const recordDataRawLength = recordData.source.length;
-
-        // No data to even process
-        if (columns.length === 0) { return []; }
-
         // The total number of records is equal to the full raw data array
         // length minus the header (1 row)
-        const numberOfRecords = recordDataRawLength - 1;
+        const numberOfRecords = this.numberOfRecords;
 
         // Filter only those with column values non-unique (i.e. drop ID-like 
         // columns)
-        return [
-            {
-                text: "-- DISABLE PARTITIONING --",
-                value: undefined,
-            },
-            ...columns
-                .filter((column) => {
-                    const valueSet = ColumnData.GetValueSet(column);
-                    return valueSet.size !== numberOfRecords;
-                })
-                .map((column) => ({
-                    text: column.label,
-                    value: ColumnData.ConvertToMinimalDescriptor(column),
-                })),
-        ];
+        return this.columns
+            .filter((column) => {
+                const valueSet = ColumnData.GetValueSet(column);
+                return valueSet.size !== numberOfRecords;
+            });
+    }
+
+    get partitionSizesPerColumn() {
+        return this.possiblePartitionColumns
+            .map((column) => {
+                return {
+                    column: ColumnData.ConvertToMinimalDescriptor(column),
+                    size: ColumnData.GetValueSet(column).size,
+                };
+            });
+    }
+
+    get bestGuessPartitionColumn() {
+        // Best guess partition column is one that isn't fully unique but not
+        // so few in number (like something based on domestic/international
+        // flag)
+        //
+        // Basic difference check used here, with the "guess" positioned around
+        // when the first sharp change in size between different columns is
+        // detected (> 5% of record data size)
+        const columnsBySize = this.partitionSizesPerColumn.sort((a, b) => a.size - b.size);
+        const deltaThreshold = (this.numberOfRecords * 0.05) >>> 0;
+
+        for (let i = 1; i < columnsBySize.length; ++i) {
+            // Return the column just before the big diff jump
+            if ((columnsBySize[i].size - columnsBySize[i - 1].size) > deltaThreshold) {
+                return columnsBySize[i - 1].column;
+            }
+        }
+
+        // If nothing picked up, then just return the middle one
+        return columnsBySize[(columnsBySize.length / 2) >>> 0].column;
+    }
+
+    get formattedPartitionColumnList() {
+        const formattedList = this.possiblePartitionColumns
+            .map((column) => ({
+                text: column.label,
+                value: ColumnData.ConvertToMinimalDescriptor(column),
+            }));
+
+        if (this.enablePartitioning) {
+            // Return the list of possible columns
+            return formattedList;
+
+        } else {
+            // Return a placeholder column list where the value is `undefined`
+            // for the last set column, or the best guess if that is not
+            // available
+            const lastSetColumn = this.lastSetColumn || this.bestGuessPartitionColumn;
+
+            return formattedList.map((x) => {
+                if (x.value._id === lastSetColumn._id) {
+                    return {
+                        ...x,
+                        value: undefined,
+                    };
+                } else {
+                    return x;
+                }
+            });
+        }
+    }
+
+    get enablePartitioning() {
+        return S.state.recordData.partitionColumn !== undefined;
+    }
+
+    set enablePartitioning(val: boolean) {
+        if (val) {
+            // Set the last known column or the best guess
+            this.partitionColumn = this.lastSetColumn || this.bestGuessPartitionColumn;
+        } else {
+            this.partitionColumn = undefined;
+        }
     }
 
     get partitionColumn(): IColumnData_MinimalDescriptor | undefined {
@@ -103,8 +182,14 @@ export default class PoolRecords extends Mixin(AnnealProcessWizardPanel) {
         if (val === undefined) {
             S.dispatch(S.action.CLEAR_RECORD_PARTITION_COLUMN, undefined);
         } else {
+            this.lastSetColumn = val;
             S.dispatch(S.action.SET_RECORD_PARTITION_COLUMN, val);
         }
+    }
+
+    @Lifecycle mounted() {
+        // Init the last set column with the partition column set in the state
+        this.lastSetColumn = S.state.recordData.partitionColumn;
     }
 }
 </script>
@@ -122,5 +207,62 @@ export default class PoolRecords extends Mixin(AnnealProcessWizardPanel) {
 .example-table td {
     border: 1px solid #aaa;
     padding: 0.1em 0.3em;
+}
+
+.pool-records-split-pane-wrapper {
+    display: block;
+    margin: 1rem 0;
+}
+
+.pool-records-split-pane {
+    display: flex;
+    flex-direction: row;
+    align-items: stretch;
+    align-content: flex-start;
+}
+
+.pool-records-section-box {
+    border: 1px solid #49075E;
+    background: rgba(73, 7, 94, 0.1);
+    padding: 1.2em;
+    margin-left: 1em;
+
+    position: relative;
+    z-index: 0;
+
+    flex-basis: 0;
+    flex-shrink: 0;
+    flex-grow: 1;
+
+    width: 35em;
+    max-width: 35em;
+    overflow: hidden;
+}
+
+.pool-records-section-box:first-child {
+    margin-left: 0;
+}
+
+.pool-records-section-box::before {
+    display: inline-block;
+    width: 0.9em;
+    text-align: center;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    font-size: 4em;
+    font-weight: bold;
+    line-height: 1;
+    color: #fff;
+}
+
+.pool-records-section-box.deselected {
+    border-color: rgba(50, 50, 50, 0.2);
+    background: rgba(50, 50, 50, 0.1);
+}
+
+.pool-records-section-box>h3 {
+    margin-top: 0;
 }
 </style>
